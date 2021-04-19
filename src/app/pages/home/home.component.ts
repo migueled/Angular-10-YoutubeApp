233cbf168309ccd75e5722379e1e3bc1126258fd
MiguelED <migueled2695@gmail.com>
2021-04-19 17:20:14 -0500

    mostrar video con modal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
 import { YoutubeResponse, Video } from '../../models/youtube.model';
 
+import Swal from 'sweetalert2'
+
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
@@ -17,8 +19,30 @@ export class HomeComponent implements OnInit {
         this.youtubeservice.getVideos().subscribe(
             data => {
               this.videos.push( ... data );
+              console.log( this.videos );
             }
         );
+    }
+
+    mostrarVideo( video : Video ) {
+        Swal.fire({
+            html :`
+            <h4>${ video.title }</h4>
+            <hr>
+            <iframe width="100%"
+                        height="315" 
+                        src="https://www.youtube.com/embed/${ video.resourceId.videoId }"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer;
+                        autoplay;
+                        clipboard-write;
+                        encrypted-media;
+                        gyroscope;
+                        picture-in-picture"
+                        allowfullscreen>
+                    </iframe>`
+        });
     }
 
 }
