98f9ebb2d6b515a4fac8b0ea93ede77d680f0d11
MiguelED <migueled2695@gmail.com>
2021-04-19 02:48:47 -0500

    Servicio para obtener videos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 
+import { HttpClientModule } from '@angular/common/http'
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class YoutubeService {
+import { HttpClient , HttpParams } from '@angular/common/http';
 
-  constructor() { }
+@Injectable({
+    providedIn: 'root'
+})
+
+export class YoutubeService {
+    //?part=snippet&=&=&=10
+    private youtubeUrl    : string = 'https://www.googleapis.com/youtube/v3';
+    private apiKey        : string = 'AIzaSyBHLZzytcylRRpuFepi2mHyMD3Ede-r7-s';
+    private playList      : string = 'UUuaPTYj15JSkETGnEseaFFg';
+    private nextPageToken : string = '';
+
+    constructor( private http : HttpClient ) { }
+
+    getVideos() {
+        const url = `${ this.youtubeUrl }/playlistItems`
+        const params = new HttpParams()
+        .set( 'part' ,         'snippet' )
+        .set( 'maxResults' ,   '10' )
+        .set( 'playlistId' ,   this.playList )
+        .set( 'key' ,          this.apiKey );
+
+        return this.http.get( url , { params } );
+    }
 }
